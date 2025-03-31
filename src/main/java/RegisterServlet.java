bd781064f07ff4cbfe373466b507fc7994a0e27b
MyShip <nichijouhappe6848@gmail.com>
2025-03-31 13:56:23 +0900

    commit_1

---
+++ src/main/java/RegisterServlet.java
@@ -0,0 +1,50 @@
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import bean.RegisterBean;
+
+@WebServlet("/register")
+public class RegisterServlet extends HttpServlet {
+  public void doPost(HttpServletRequest req, HttpServletResponse res)
+      throws IOException, ServletException {
+    // ���N�G�X�g�̕����R�[�h�w��
+    req.setCharacterEncoding("utf-8");
+    
+    // ���͏��̎擾
+    String name = req.getParameter("name");
+    String age = req.getParameter("age");
+    String[] langs = req.getParameterValues("lang");
+    String money  = req.getParameter("money");
+    String number1 = req.getParameter("number1");
+    int num1 = Integer.parseInt(number1);
+    String number2 = req.getParameter("number2");
+    int num2 = Integer.parseInt(number2);
+    res.setContentType("text/html;charset=utf-8");
+    
+    // Bean�̍쐬
+    RegisterBean rb = new RegisterBean();
+    rb.setName(name);
+    rb.setAge(age);
+    rb.setLangs(langs);
+    rb.setMoney(money);
+    rb.setNumber1(num1);
+    rb.setNumber2(num2);
+    
+    // Bean�����N�G�X�g�Ɋi�[
+    req.setAttribute("rb", rb);
+    
+    // register.jsp�փt�H���[�h
+    RequestDispatcher rd = req.getRequestDispatcher("/register.jsp");
+    rd.forward(req, res);
+  }
+  public void doGet(HttpServletRequest req, HttpServletResponse res)
+      throws IOException, ServletException {
+    doPost(req, res);
+  }
+}
